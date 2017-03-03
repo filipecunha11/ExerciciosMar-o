778d8c8438155415ec89e62095f3d0d71f2e86ff
Filipe <Filipe@10.0.2.15>
2017-03-03 09:41:58 +0000

    Pequenas alterações

--- src/mar03/Exercicio1.java
+++ src/mar03/Exercicio1.java
@@ -13,37 +13,31 @@ public class Exercicio1 {
 		scanner = new Scanner(System.in);
 		do {
 			System.out.println("\nMenu ");
-			System.out.println("1. Inserir");
-			System.out.println("2. Eliminar");
-			System.out.println("3. Verificar");
-			System.out.print("0. Sair\n> ");
+			System.out.println("1. Inserir\n2. Eliminar\n3. Verificar\n\n> ");
 			
 			int menu = scanner.nextInt();
 			
-			if (menu == 1)
+			switch (menu)
 			{
+			case 1:
 				System.out.print("\nInsira um numero: ");
 				num = scanner.nextInt();
 				addNumber(num);
-			}
-			else if (menu == 2)
-			{
+				break;
+			case 2:
 				System.out.print("\nInsira a posição a eliminar: ");
 				num = scanner.nextInt();
 				delNum(num);
-			}
-			else if (menu == 3)
-				showArray(est);
-			else if (menu == 0)
 				break;
+			case 3:
+				showArray(est);
+				break;
+			}
 		} while (true);
 	}
 	
 	private void showArray(int[] arr) {
-		System.out.print("\n\nArray: ");
-		for (int i = 0 ; i < arr.length-1 ; i++)
-			System.out.print(arr[i] + ", ");
-		System.out.print(arr[arr.length-1]);
+		System.out.println("\nArray: " + Arrays.toString(arr));
 	}
 
 	public int[] addNumber(int newNumber) {
@@ -54,11 +48,8 @@ public class Exercicio1 {
 	
 	public int[] delNum (int pos){
 		for (int i = pos ; i < est.length  ; i++ )
-		{
 			if (i != est.length-1)
 				est[i] = est[i+1];
-		}
-		est = Arrays.copyOf(est, est.length-1);
-		return est;
+		return est = Arrays.copyOf(est, est.length-1);
 	}
 }
